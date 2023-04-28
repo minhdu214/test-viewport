e3601cfc267b22ff39764e5a2ab33eec7550e812
Chriskatalon <quan.trinh@katalon.com>
2023-04-28 16:18:32 +0700

    hi

---
+++ Object Repository/Page_Udaan/div_Login                                  _cdf38e.rs
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Login                                  _cdf38e</name>
+   <tag></tag>
+   <elementGuidId>c13d9a20-7125-4ab6-afd8-c1138baecbee</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='page-content-wrapper']/div[2]/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.container.login_home</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>d80b5672-8ba2-40ca-aa1e-e14b3fba9c38</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>container login_home</value>
+      <webElementGuid>b404dd9d-e4cb-4fe3-ba4d-d3aeb4f5098c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+		
+			
+				
+			        
+			            
+			        
+			    
+				
+	                
+	                    
+	                    
+	                
+	            
+				Login
+			
+
+			
+
+    
+
+        
+        
+            
+              ×
+              Login As
+            
+            
+                
+            
+        
+    
+
+
+
+		
+	</value>
+      <webElementGuid>5952d3d6-decb-4aa2-9148-e0b2a12f2d29</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;page-content-wrapper&quot;)/div[@class=&quot;ng-scope&quot;]/div[@class=&quot;main-wrapper clear ng-scope&quot;]/div[@class=&quot;container login_home&quot;]</value>
+      <webElementGuid>80f6168d-f31e-49d0-b4fe-494cc91e90e9</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='page-content-wrapper']/div[2]/div/div</value>
+      <webElementGuid>77904ae7-3507-496e-80e4-538e681ce364</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='OK'])[1]/following::div[3]</value>
+      <webElementGuid>947bbca7-8faa-4d86-afe8-70adea8251ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Copied'])[1]/following::div[4]</value>
+      <webElementGuid>8092e3cb-a0cd-40b6-a08f-b9de6e74c8ac</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div</value>
+      <webElementGuid>9590b6b2-b2c6-4122-9e30-7623af25ec12</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+		
+			
+				
+			        
+			            
+			        
+			    
+				
+	                
+	                    
+	                    
+	                
+	            
+				Login
+			
+
+			
+
+    
+
+        
+        
+            
+              ×
+              Login As
+            
+            
+                
+            
+        
+    
+
+
+
+		
+	' or . = '
+		
+			
+				
+			        
+			            
+			        
+			    
+				
+	                
+	                    
+	                    
+	                
+	            
+				Login
+			
+
+			
+
+    
+
+        
+        
+            
+              ×
+              Login As
+            
+            
+                
+            
+        
+    
+
+
+
+		
+	')]</value>
+      <webElementGuid>85000b48-5c6c-47a0-a868-741c0012e5c6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Headless_Failed.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Headless_Failed</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1476489e-ac9f-4bf4-9814-aac543d7c395</testSuiteGuid>
+   <testCaseLink>
+      <guid>474617ed-c45b-40ea-a6b4-311fd1a02b2d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Headless_Failed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
